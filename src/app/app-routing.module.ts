e6e7fbfaa6bfe3f0ab1d2c3f0294635541d1c427
Anna Blendl <anna.blendl@gmx.de>
2019-04-13 20:22:47 +0200

    tabs scheiss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,21 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: './home/home.module#HomePageModule'
+    loadChildren: './platform/tabs/tabs.module#TabsPageModule'
   },
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  },
-  { path: 'home', loadChildren: './platform/home/home.module#HomePageModule' },
-  { path: 'schmoekern', loadChildren: './platform/schmoekern/schmoekern.module#SchmoekernPageModule' },
-  { path: 'anfrage', loadChildren: './platform/anfrage/anfrage.module#AnfragePageModule' },
-  { path: 'home', loadChildren: './börse/home/home.module#HomePageModule' }
+  }
 ];
 
 @NgModule({
